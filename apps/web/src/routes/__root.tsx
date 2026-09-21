0553a5434d8da8c4fa0d1a80d09b9246d8391d06
Simon Vajda <43452607+simon-vajda@users.noreply.github.com>
2026-09-21 22:08:30 +0200

    Add fallback 404 page (#48)

--- apps/web/src/routes/__root.tsx
+++ apps/web/src/routes/__root.tsx
@@ -2,10 +2,12 @@ import type { QueryClient } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { createRootRouteWithContext, Outlet } from '@tanstack/react-router';
 import { TanStackRouterDevtools } from '@tanstack/react-router-devtools';
+import { NotFoundPage } from '@/components/not-found-page';
 import { Toaster } from '@/components/ui/sonner';
 
 export const Route = createRootRouteWithContext<{ queryClient: QueryClient }>()({
   component: RootComponent,
+  notFoundComponent: NotFoundPage,
 });
 
 function RootComponent() {
